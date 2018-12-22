769c8081d90625cb67b1ef2261a9a81664f2f941
akpath <kpathan@outlook.com>
2018-12-22 11:55:28 -0500

    initial commit

---
+++ src/main/java/khyri/App.java
@@ -0,0 +1,14 @@
+package khyri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        var msg = "Hello, worm!";
+        System.out.println(msg);
+    }
+}

d71b5bca9fda9ff6b49f609e37844d5bb1442b61
akpath <kpathan@outlook.com>
2018-12-31 23:08:53 -0500

    opengl start

--- src/main/java/khyri/App.java
+++ src/main/java/khyri/App.java
@@ -1,14 +1,185 @@
 package khyri;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        var msg = "Hello, worm!";
-        System.out.println(msg);
+import static com.jogamp.opengl.GL4.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import javax.swing.JFrame;
+
+import com.jogamp.opengl.GL4;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLContext;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.glu.GLU;
+
+public class App extends JFrame implements GLEventListener {
+
+    private GLCanvas canvas;
+    private int renderingProgram;
+    private int[] vao = new int[1];
+
+    public App() {
+        setTitle("Chapter 2 - program 1");
+        setSize(600, 400);
+        setLocation(200, 200);
+        canvas = new GLCanvas();
+        canvas.addGLEventListener(this);
+        this.add(canvas);
+        setVisible(true);
+    }
+
+    @Override
+    public void init(GLAutoDrawable drawable) {
+        GL4 gl = (GL4) GLContext.getCurrentGL();
+        renderingProgram = createShaderProgram();
+        gl.glGenVertexArrays(vao.length, vao, 0);
+        gl.glBindVertexArray(vao[0]);
+    }
+
+    private int createShaderProgram() {
+        GL4 gl = (GL4) GLContext.getCurrentGL();
+
+        String[] vShaderSource = readShaderSource("vertex.glsl");
+        String[] fShaderSource = readShaderSource("fragment.glsl");
+
+        int[] vertCompiled = new int[1];
+        int[] fragCompiled = new int[1];
+        int[] linked = new int[1];
+
+        int vShader = gl.glCreateShader(GL_VERTEX_SHADER);
+        gl.glShaderSource(vShader, vShaderSource.length, vShaderSource, null, 0);
+        gl.glCompileShader(vShader);
+        checkOpenGLError();
+        gl.glGetShaderiv(vShader, GL_COMPILE_STATUS, vertCompiled, 0);
+        if (vertCompiled[0] == 1) {
+            System.out.println("... vertex compilation success.");
+        } else {
+            System.out.println("... vertex compilation failed.");
+            printShaderLog(vShader);
+        }
+
+        int fShader = gl.glCreateShader(GL_FRAGMENT_SHADER);
+        gl.glShaderSource(fShader, fShaderSource.length, fShaderSource, null, 0);
+        gl.glCompileShader(fShader);
+        checkOpenGLError();
+        gl.glGetShaderiv(fShader, GL_COMPILE_STATUS, fragCompiled, 0);
+        if (fragCompiled[0] == 1) {
+            System.out.println("... fragment compilation success.");
+        } else {
+            System.out.println("... fragment compilation failed.");
+            printShaderLog(fShader);
+        }
+
+        int vfprogram = gl.glCreateProgram();
+        gl.glAttachShader(vfprogram, vShader);
+        gl.glAttachShader(vfprogram, fShader);
+        gl.glLinkProgram(vfprogram);
+        checkOpenGLError();
+        gl.glGetProgramiv(vfprogram, GL_LINK_STATUS, linked, 0);
+        if (linked[0] == 1) {
+            System.out.println("... linking succeeded.");
+        } else {
+            System.out.println("... linking failed.");
+            printProgramLog(vfprogram);
+        }
+
+        gl.glDeleteShader(vShader);
+        gl.glDeleteShader(fShader);
+
+        return vfprogram;
+    }
+
+    private void printShaderLog(int shader) {
+        GL4 gl = (GL4) GLContext.getCurrentGL();
+        int[] len = new int[1];
+        int[] chWrittn = new int[1];
+        byte[] log = null;
+        // determine the length of the shader compilation log
+        gl.glGetShaderiv(shader, GL_INFO_LOG_LENGTH, len, 0);
+        if (len[0] > 0) {
+            log = new byte[len[0]];
+            gl.glGetShaderInfoLog(shader, len[0], chWrittn, 0, log, 0);
+            System.out.println("Shader Info Log: ");
+            for (int i = 0; i < log.length; i++) {
+                System.out.print((char) log[i]);
+            }
+        }
+    }
+
+    void printProgramLog(int prog) {
+        GL4 gl = (GL4) GLContext.getCurrentGL();
+        int[] len = new int[1];
+        int[] chWrittn = new int[1];
+        byte[] log = null;
+        // determine the length of the program linking log
+        gl.glGetProgramiv(prog, GL_INFO_LOG_LENGTH, len, 0);
+        if (len[0] > 0) {
+            log = new byte[len[0]];
+            gl.glGetProgramInfoLog(prog, len[0], chWrittn, 0, log, 0);
+            System.out.println("Program Info Log: ");
+            for (int i = 0; i < log.length; i++) {
+                System.out.print((char) log[i]);
+            }
+        }
+    }
+
+    boolean checkOpenGLError() {
+        GL4 gl = (GL4) GLContext.getCurrentGL();
+        boolean foundError = false;
+        GLU glu = new GLU();
+        int glErr = gl.glGetError();
+        while (glErr != GL_NO_ERROR) {
+            System.err.println("glError: " + glu.gluErrorString(glErr));
+            foundError = true;
+            glErr = gl.glGetError();
+        }
+        return foundError;
+    }
+
+    private String[] readShaderSource(String filename) {
+        URI resource;
+        try {
+
+            resource = ClassLoader.getSystemResource(filename).toURI();
+
+            try (Stream<String> lines = Files.lines(Paths.get(resource))) {
+                return lines.map(l -> l + "\n").toArray(String[]::new);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                return null;
+            }
+
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public void display(GLAutoDrawable drawable) {
+        GL4 gl = (GL4) GLContext.getCurrentGL();
+        gl.glUseProgram(renderingProgram);
+        gl.glPointSize(30.0f);
+        gl.glDrawArrays(GL_POINTS, 0, 1);
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable drawable) {
+
+    }
+
+    public static void main(String[] args) {
+        new App();
     }
 }

46ab70c7a84f9a830d183bb19336818a04aa42ca
akpath <kpathan@outlook.com>
2019-01-05 14:59:26 -0500

    Use built-in utilities for shader loading

--- src/main/java/khyri/App.java
+++ src/main/java/khyri/App.java
@@ -20,6 +20,8 @@ import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.opengl.glu.GLU;
 import com.jogamp.opengl.util.FPSAnimator;
+import com.jogamp.opengl.util.glsl.ShaderCode;
+import com.jogamp.opengl.util.glsl.ShaderProgram;
 
 public class App extends JFrame implements GLEventListener {
 
@@ -46,128 +48,25 @@ public class App extends JFrame implements GLEventListener {
     @Override
     public void init(GLAutoDrawable drawable) {
         GL4 gl = (GL4) GLContext.getCurrentGL();
-        renderingProgram = createShaderProgram();
+        renderingProgram = createShaderProgram(gl);
         gl.glGenVertexArrays(vao.length, vao, 0);
         gl.glBindVertexArray(vao[0]);
     }
 
-    private int createShaderProgram() {
-        GL4 gl = (GL4) GLContext.getCurrentGL();
-
-        String[] vShaderSource = readShaderSource("vertex.glsl");
-        String[] fShaderSource = readShaderSource("fragment.glsl");
-
-        int[] vertCompiled = new int[1];
-        int[] fragCompiled = new int[1];
-        int[] linked = new int[1];
-
-        int vShader = gl.glCreateShader(GL_VERTEX_SHADER);
-        gl.glShaderSource(vShader, vShaderSource.length, vShaderSource, null, 0);
-        gl.glCompileShader(vShader);
-        checkOpenGLError();
-        gl.glGetShaderiv(vShader, GL_COMPILE_STATUS, vertCompiled, 0);
-        if (vertCompiled[0] == 1) {
-            System.out.println("... vertex compilation success.");
-        } else {
-            System.out.println("... vertex compilation failed.");
-            printShaderLog(vShader);
-        }
-
-        int fShader = gl.glCreateShader(GL_FRAGMENT_SHADER);
-        gl.glShaderSource(fShader, fShaderSource.length, fShaderSource, null, 0);
-        gl.glCompileShader(fShader);
-        checkOpenGLError();
-        gl.glGetShaderiv(fShader, GL_COMPILE_STATUS, fragCompiled, 0);
-        if (fragCompiled[0] == 1) {
-            System.out.println("... fragment compilation success.");
-        } else {
-            System.out.println("... fragment compilation failed.");
-            printShaderLog(fShader);
-        }
-
-        int vfprogram = gl.glCreateProgram();
-        gl.glAttachShader(vfprogram, vShader);
-        gl.glAttachShader(vfprogram, fShader);
-        gl.glLinkProgram(vfprogram);
-        checkOpenGLError();
-        gl.glGetProgramiv(vfprogram, GL_LINK_STATUS, linked, 0);
-        if (linked[0] == 1) {
-            System.out.println("... linking succeeded.");
-        } else {
-            System.out.println("... linking failed.");
-            printProgramLog(vfprogram);
-        }
-
-        gl.glDeleteShader(vShader);
-        gl.glDeleteShader(fShader);
-
-        return vfprogram;
-    }
-
-    private void printShaderLog(int shader) {
-        GL4 gl = (GL4) GLContext.getCurrentGL();
-        int[] len = new int[1];
-        int[] chWrittn = new int[1];
-        byte[] log = null;
-        // determine the length of the shader compilation log
-        gl.glGetShaderiv(shader, GL_INFO_LOG_LENGTH, len, 0);
-        if (len[0] > 0) {
-            log = new byte[len[0]];
-            gl.glGetShaderInfoLog(shader, len[0], chWrittn, 0, log, 0);
-            System.out.println("Shader Info Log: ");
-            for (int i = 0; i < log.length; i++) {
-                System.out.print((char) log[i]);
-            }
-        }
-    }
-
-    void printProgramLog(int prog) {
-        GL4 gl = (GL4) GLContext.getCurrentGL();
-        int[] len = new int[1];
-        int[] chWrittn = new int[1];
-        byte[] log = null;
-        // determine the length of the program linking log
-        gl.glGetProgramiv(prog, GL_INFO_LOG_LENGTH, len, 0);
-        if (len[0] > 0) {
-            log = new byte[len[0]];
-            gl.glGetProgramInfoLog(prog, len[0], chWrittn, 0, log, 0);
-            System.out.println("Program Info Log: ");
-            for (int i = 0; i < log.length; i++) {
-                System.out.print((char) log[i]);
-            }
-        }
-    }
-
-    boolean checkOpenGLError() {
-        GL4 gl = (GL4) GLContext.getCurrentGL();
-        boolean foundError = false;
-        GLU glu = new GLU();
-        int glErr = gl.glGetError();
-        while (glErr != GL_NO_ERROR) {
-            System.err.println("glError: " + glu.gluErrorString(glErr));
-            foundError = true;
-            glErr = gl.glGetError();
-        }
-        return foundError;
-    }
-
-    private String[] readShaderSource(String filename) {
-        URI resource;
-        try {
-
-            resource = ClassLoader.getSystemResource(filename).toURI();
-
-            try (Stream<String> lines = Files.lines(Paths.get(resource))) {
-                return lines.map(l -> l + "\n").toArray(String[]::new);
-            } catch (IOException ex) {
-                ex.printStackTrace();
-                return null;
-            }
-
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            return null;
-        }
+    private int createShaderProgram(GL4 gl) {
+        ShaderCode vertexShader = ShaderCode.create(gl, GL_VERTEX_SHADER, getClass(), "shaders", null, "vertex", "glsl", null, true);
+        ShaderCode fragmentShader = ShaderCode.create(gl, GL_FRAGMENT_SHADER, getClass(), "shaders", null, "fragment", "glsl", null, true);
+        
+        ShaderProgram program = new ShaderProgram();
+        
+        program.add(vertexShader);
+        program.add(fragmentShader);
+        
+        program.init(gl);
+        
+        program.link(gl, System.err);
+        
+        return program.program();
     }
 
     @Override
